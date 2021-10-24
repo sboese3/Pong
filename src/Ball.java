f0aaa35bd69eadc879c753afbc288f599c6c4a1d
sboese1 <samboese3@gmail.com>
2021-10-23 22:12:25 -0500

    Window + drawings
    Created the window with the drawings of the paddles and ball. Still needs the ability to move. Also I added the "Panel" class just so it's easier to separate the frame and panel

--- src/Ball.java
+++ src/Ball.java
@@ -1,3 +1,18 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import java.util.*;
+
 public class Ball {
 
+    int radius = 20;
+
+    Ball() {
+
+    }
+
+    public void draw(Graphics g) {
+        g.setColor(Color.white);
+        g.fillOval(750, 375, radius, radius);
+    }
 }
