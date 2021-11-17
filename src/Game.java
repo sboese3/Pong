dd58dbe3926e54f6b8ad51b05b701752bef6dca7
jwoods7097 <jwoods7097@gmail.com>
2021-11-17 12:08:53 -0600

    Added score display

--- src/Game.java
+++ src/Game.java
@@ -8,6 +8,9 @@ import java.io.IOException;
 
 public class Game extends JPanel implements ActionListener {
 
+    JLabel p1;
+    JLabel p2;
+
     // Game updates 60 times per second, can change if needed
     Timer timer = new Timer(1000 / 60, this);
 
@@ -15,14 +18,37 @@ public class Game extends JPanel implements ActionListener {
     private Paddle leftPaddle;
     private Paddle rightPaddle;
 
+    private int p1Score;
+    private int p2Score;
+
+    // Initial setup of graphics and game settings
     public Game() {
         timer.start();
+
+        p1Score = 0;
+        p2Score = 0;
+
+        this.setLayout(null);
+        p1 = new JLabel("" + p1Score);
+        p1.setSize(60, 60);
+        p1.setLocation(450, 0);
+        p1.setFont(new Font(p1.getFont().getName(), Font.PLAIN, 60));
+        p1.setForeground(Color.white);
+        this.add(p1);
+
+        p2 = new JLabel("" + p2Score);
+        p2.setSize(60, 60);
+        p2.setLocation(540, 0);
+        p2.setFont(new Font(p2.getFont().getName(), Font.PLAIN, 60));
+        p2.setForeground(Color.white);
+        this.add(p2);
 
         ball = new Ball();
         leftPaddle = new Paddle(0);
         rightPaddle = new Paddle(Window.WINDOW_WIDTH - Paddle.WIDTH);
     }
 
+    // Paints background and game objects
     @Override
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
@@ -36,14 +62,13 @@ public class Game extends JPanel implements ActionListener {
             System.out.println("Could not load background!");
         }
 
-        // Draw game objects
-        draw(g2d);
-    }
+        p1.setText("" + p1Score);
+        p2.setText("" + p2Score);
 
-    public void draw(Graphics2D g) {
-        ball.draw(g);
-        leftPaddle.draw(g);
-        rightPaddle.draw(g);
+        // Draw game objects
+        ball.draw(g2d);
+        leftPaddle.draw(g2d);
+        rightPaddle.draw(g2d);
     }
 
     // This method is called every time the game updates
